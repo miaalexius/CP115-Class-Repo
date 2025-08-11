8b8b7f9446af94e66684f1561bfbd5addd42497b
miaalexius <miadaziy@icloud.com>
2025-08-11 11:58:52 +0800

    LAB 6 DONEEEEEEE

---
+++ labs/lab06/exercise2.py
@@ -0,0 +1,15 @@
+message_1 = 'imanie said "Good Morning mia!" every single day'
+message_2 = "Asna's favourite food is Ayam Geprek, she eat it every single day"
+
+long_text_1 = ''' "i wanna be defined by the things that i loved, 
+not the things that i hate 
+nor the things im afraid of" 
+- Daylight Taylor swift'''
+
+long_text_2 = """I prefer Taylor's version more than the original one
+but i like Better Than Revenge original version more"""
+
+print(message_1)
+print(message_2)
+print(long_text_1)
+print(long_text_2)
