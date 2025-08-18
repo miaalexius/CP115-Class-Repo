fec93b274b180d20e5e8aefa413afdbbc9f28ab0
miaalexius <miadaziy@icloud.com>
2025-08-18 13:10:36 +0800

    Add lab07 exercises and update .gitignore

---
+++ labs/lab07/exercise3.py
@@ -0,0 +1,8 @@
+import shopping_data
+
+print (f"Product Name: {shopping_data.product_name}")
+print (f"Product Price: {shopping_data.price}")
+print (f"Product Quantity: {shopping_data.quantity}")
+print(f"Product in UPPERCASE: {shopping_data.straw_name}")
+print(f"Product in lowercase: {shopping_data.berry_name}")
+print(f"Name length: {shopping_data.length_name} characters")
---
+++ labs/lab07/shopping_data.py
@@ -0,0 +1,10 @@
+#Product's informations
+product_name = "Extra Flavoured Strawberry Milk"
+price = 2.50
+quantity = 100
+
+total_cost = price * quantity
+
+straw_name = product_name.upper()      
+berry_name = product_name.lower()      
+length_name = len(product_name)            
