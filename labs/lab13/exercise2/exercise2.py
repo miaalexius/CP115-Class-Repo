f595ef8d94fcb594073bbeb5f72c6e339b0e01b0
miaalexius <miadaziy@icloud.com>
2025-10-27 12:57:17 +0800

    fdsf

--- labs/lab13/exercise2/exercise2.py
+++ labs/lab13/exercise2/exercise2.py
@@ -1,3 +1,7 @@
-# TODO: Your code here
+for number in range(1,101) : 
+    print(f'Checking: {number}')
+    if number % 7 == 0 and number % 13 == 0:
+        break  # Stop searching
 
+found_number = number
 print(found_number)
